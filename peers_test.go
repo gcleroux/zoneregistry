ad30107eec61dd77947c3e11a8543bd1b43bc149
Guillaume Cléroux <73357644+gcleroux@users.noreply.github.com>
2025-01-22 18:46:13 -0500

    add Makefile

---
+++ peers_test.go
@@ -0,0 +1,27 @@
+package zoneregistry
+
+import (
+	"testing"
+)
+
+func TestPeers(t *testing.T) {
+	tests := []struct {
+		input            string
+		expectedPeerHost string
+	}{
+		{
+			input:            `example.org`,
+			expectedPeerHost: "example.org.",
+		},
+	}
+
+	for i, test := range tests {
+		peer := NewPeer(test.input)
+
+		// Validate Peer Host
+		if test.expectedPeerHost != peer.Host {
+			t.Errorf("Test %d, expected %s host for input %s, got: %s", i, test.expectedPeerHost, test.input, peer.Host)
+		}
+
+	}
+}
